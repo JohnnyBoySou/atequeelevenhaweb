8dd59f6953b500a8dbc5d9f15593513383116461
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-15 01:24:43 -0300

    orinal atuliz

--- app/data/shorts/original.js
+++ app/data/shorts/original.js
@@ -1,20 +1,17 @@
 export const Shorts_Original = [
     {
-        id: 1, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash2.png' 
+        id: 1, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg' 
     },
     {
-        id: 2, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash3.png'
+        id: 2, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/43/17/36/431736564cb0cfaad832c833f6681ccc.jpg'
     },
     {
-        id: 3, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash4.png'
+        id: 3, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/98/86/a3/9886a32d8e170c9e098f31c4a7b57fd0.jpg'
     },
     {
-        id: 4, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash5.png'
+        id: 4, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/95/c3/38/95c3388547a2ae9609087c460a503482.jpg'
     },
     {
-        id: 5, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash6.png'
+        id: 5, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/2a/e6/a0/2ae6a0f097217bb0cd0d0ed676e58b32.jpg'
     },
-    {
-        id: 6, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://github.com/JohnnyBoySou/atequeelevenhaweb/blob/master/app/assets/splash7.png'
-    }
 ]
