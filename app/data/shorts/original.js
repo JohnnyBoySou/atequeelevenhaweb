f626a67d3975585e682418c928283f3c582d1af1
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-16 10:37:28 -0300

    new date

--- app/data/shorts/original.js
+++ app/data/shorts/original.js
@@ -1,9 +1,11 @@
 export const Shorts_Original = [
+  
     {
-        id: 1, title: 'Como vencer a ansiedade', desc: 'Vencer a ansiedade pode ser um desafio, mas existem várias estratégias que podem ajudar: \n\nRespiração profunda: Praticar técnicas de respiração profunda pode acalmar seu sistema nervoso e reduzir a ansiedade. Experimente respirar lenta e profundamente, concentrando-se na sua respiração. \n\n\ Exercício físico: O exercício regular é uma ótima maneira de aliviar o estresse e a ansiedade. Ele libera endorfinas, que são neurotransmissores que ajudam a melhorar o humor e reduzir a ansiedade.\n\n\Meditação e mindfulness: Praticar meditação e mindfulness pode ajudar a acalmar a mente e reduzir a ansiedade. Essas práticas ensinam a focar no momento presente, em vez de se preocupar com o futuro.', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/5a/a1/5f/5aa15f827aad1c9dfc42529dcb4c038d.mp4'
-    },
-    {
-        id: 2, title: '5 habitos para está com Deus', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/43/17/36/431736564cb0cfaad832c833f6681ccc.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/23/20/24/232024a6f07f4bd42b24d76d85f42011.mp4'
+        id: 5, 
+        title: 'Um dia a mais para vencer', 
+        desc: '...e a cada dia que passa, Deus nos dá forças para vencer.', 
+        url: 'https://i.pinimg.com/videos/thumbnails/originals/23/20/24/232024a6f07f4bd42b24d76d85f42011.0000000.jpg', 
+        video: 'https://v1.pinimg.com/videos/mc/720p/23/20/24/232024a6f07f4bd42b24d76d85f42011.mp4'
     },
     {
         id: 6,
