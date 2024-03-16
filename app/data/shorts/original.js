6131118b2cedda7a3d30218a95e4b8f72bd468fd
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-15 23:32:25 -0300

    new ideos

--- app/data/shorts/original.js
+++ app/data/shorts/original.js
@@ -1,17 +1,17 @@
 export const Shorts_Original = [
     {
-        id: 1, title: 'Como vencer a ansiedade', desc: 'Vencer a ansiedade pode ser um desafio, mas existem várias estratégias que podem ajudar: \n\nRespiração profunda: Praticar técnicas de respiração profunda pode acalmar seu sistema nervoso e reduzir a ansiedade. Experimente respirar lenta e profundamente, concentrando-se na sua respiração. \n\n\ Exercício físico: O exercício regular é uma ótima maneira de aliviar o estresse e a ansiedade. Ele libera endorfinas, que são neurotransmissores que ajudam a melhorar o humor e reduzir a ansiedade.\n\n\Meditação e mindfulness: Praticar meditação e mindfulness pode ajudar a acalmar a mente e reduzir a ansiedade. Essas práticas ensinam a focar no momento presente, em vez de se preocupar com o futuro.', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg' 
+        id: 1, title: 'Como vencer a ansiedade', desc: 'Vencer a ansiedade pode ser um desafio, mas existem várias estratégias que podem ajudar: \n\nRespiração profunda: Praticar técnicas de respiração profunda pode acalmar seu sistema nervoso e reduzir a ansiedade. Experimente respirar lenta e profundamente, concentrando-se na sua respiração. \n\n\ Exercício físico: O exercício regular é uma ótima maneira de aliviar o estresse e a ansiedade. Ele libera endorfinas, que são neurotransmissores que ajudam a melhorar o humor e reduzir a ansiedade.\n\n\Meditação e mindfulness: Praticar meditação e mindfulness pode ajudar a acalmar a mente e reduzir a ansiedade. Essas práticas ensinam a focar no momento presente, em vez de se preocupar com o futuro.', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/5a/a1/5f/5aa15f827aad1c9dfc42529dcb4c038d.mp4' 
     },
     {
-        id: 2, title: '5 habitos para está com Deus', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/43/17/36/431736564cb0cfaad832c833f6681ccc.jpg'
+        id: 2, title: '5 habitos para está com Deus', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/43/17/36/431736564cb0cfaad832c833f6681ccc.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/23/20/24/232024a6f07f4bd42b24d76d85f42011.mp4'
     },
     {
-        id: 3, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/98/86/a3/9886a32d8e170c9e098f31c4a7b57fd0.jpg'
+        id: 3, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/98/86/a3/9886a32d8e170c9e098f31c4a7b57fd0.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/04/c9/24/04c9244c8db44dd3e4314b036a896170.mp4'
     },
     {
-        id: 4, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/95/c3/38/95c3388547a2ae9609087c460a503482.jpg'
+        id: 4, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/95/c3/38/95c3388547a2ae9609087c460a503482.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/51/3b/0f/513b0f1d2ac94207fb5c77ce96771446.mp4'
     },
     {
-        id: 5, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/2a/e6/a0/2ae6a0f097217bb0cd0d0ed676e58b32.jpg'
+        id: 5, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/2a/e6/a0/2ae6a0f097217bb0cd0d0ed676e58b32.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/06/c2/11/06c2119361cd1f625fa8711f3dc525cb.mp4'
     },
 ]
