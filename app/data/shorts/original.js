43344bd9cd29a586ffedf5a7602c4e601fb1012c
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-16 10:18:21 -0300

    new date

--- app/data/shorts/original.js
+++ app/data/shorts/original.js
@@ -1,17 +1,57 @@
 export const Shorts_Original = [
     {
-        id: 1, title: 'Como vencer a ansiedade', desc: 'Vencer a ansiedade pode ser um desafio, mas existem várias estratégias que podem ajudar: \n\nRespiração profunda: Praticar técnicas de respiração profunda pode acalmar seu sistema nervoso e reduzir a ansiedade. Experimente respirar lenta e profundamente, concentrando-se na sua respiração. \n\n\ Exercício físico: O exercício regular é uma ótima maneira de aliviar o estresse e a ansiedade. Ele libera endorfinas, que são neurotransmissores que ajudam a melhorar o humor e reduzir a ansiedade.\n\n\Meditação e mindfulness: Praticar meditação e mindfulness pode ajudar a acalmar a mente e reduzir a ansiedade. Essas práticas ensinam a focar no momento presente, em vez de se preocupar com o futuro.', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/5a/a1/5f/5aa15f827aad1c9dfc42529dcb4c038d.mp4' 
+        id: 1, title: 'Como vencer a ansiedade', desc: 'Vencer a ansiedade pode ser um desafio, mas existem várias estratégias que podem ajudar: \n\nRespiração profunda: Praticar técnicas de respiração profunda pode acalmar seu sistema nervoso e reduzir a ansiedade. Experimente respirar lenta e profundamente, concentrando-se na sua respiração. \n\n\ Exercício físico: O exercício regular é uma ótima maneira de aliviar o estresse e a ansiedade. Ele libera endorfinas, que são neurotransmissores que ajudam a melhorar o humor e reduzir a ansiedade.\n\n\Meditação e mindfulness: Praticar meditação e mindfulness pode ajudar a acalmar a mente e reduzir a ansiedade. Essas práticas ensinam a focar no momento presente, em vez de se preocupar com o futuro.', url: 'https://i.pinimg.com/736x/05/25/dd/0525dd4af0c6e038eef7bf13b09f8e87.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/5a/a1/5f/5aa15f827aad1c9dfc42529dcb4c038d.mp4'
     },
     {
         id: 2, title: '5 habitos para está com Deus', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/43/17/36/431736564cb0cfaad832c833f6681ccc.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/23/20/24/232024a6f07f4bd42b24d76d85f42011.mp4'
     },
     {
-        id: 3, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/98/86/a3/9886a32d8e170c9e098f31c4a7b57fd0.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/04/c9/24/04c9244c8db44dd3e4314b036a896170.mp4'
+        id: 6,
+        title: 'És invencível',
+        desc: 'Deus é bom o tempo todo',
+        url: 'https://i.pinimg.com/564x/7f/88/0f/7f880f0786126cc6440d6644ee50d8fd.jpg',
+        video: 'https://v1.pinimg.com/videos/mc/720p/92/49/6c/92496cfbcb1aed7c54f4fc54cc5a244a.mp4',
     },
     {
-        id: 4, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/95/c3/38/95c3388547a2ae9609087c460a503482.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/51/3b/0f/513b0f1d2ac94207fb5c77ce96771446.mp4'
+        id: 7,
+        title: 'O senhor diz para você hoje',
+        desc: 'Não desista daquilo que eu te convidei',
+        url: 'https://i.pinimg.com/564x/99/39/71/993971125c50963c2167bb7714f83dc5.jpg',
+        video: 'https://v1.pinimg.com/videos/mc/720p/5c/de/6a/5cde6a7880f8f874c0bff5a613764be9.mp4',
     },
     {
-        id: 5, title: 'Como vencer a ansiedade', desc: 'para vencer a ansiadede', url: 'https://i.pinimg.com/736x/2a/e6/a0/2ae6a0f097217bb0cd0d0ed676e58b32.jpg', video: 'https://v1.pinimg.com/videos/mc/720p/06/c2/11/06c2119361cd1f625fa8711f3dc525cb.mp4'
+        id: 8,
+        title: 'Você ora?',
+        desc: 'E você não sente que tem alguem do outro lado',
+        url: "https://i.pinimg.com/videos/thumbnails/originals/91/43/b7/9143b73819acf588859c0094cbfee716.0000000.jpg",
+        video: 'https://v1.pinimg.com/videos/mc/720p/91/43/b7/9143b73819acf588859c0094cbfee716.mp4',
+    },
+    {
+        id: 9, 
+        title: 'Motivação diária',
+        desc: 'Se motive para essa semana',
+        url: 'https://i.pinimg.com/videos/thumbnails/originals/73/3a/22/733a22b9ca028f784c75d987a051fc42.000000.jpg',
+        video: 'https://v1.pinimg.com/videos/mc/720p/73/3a/22/733a22b9ca028f784c75d987a051fc42.mp4'
+    },
+    {
+        id: 10, 
+        title: 'Ele fortalece o cansado ',
+        desc: 'E dá grande vigor ao que está sem forças. Isaías 40:29',
+        url: 'https://i.pinimg.com/videos/thumbnails/originals/ba/1a/5c/ba1a5c77549a61aaaac112f207c1682f.0000000.jpg',
+        video: 'https://v1.pinimg.com/videos/mc/720p/ba/1a/5c/ba1a5c77549a61aaaac112f207c1682f.mp4'
+    },
+    {
+        id: 11, 
+        title: 'Jesus é a salvação',
+        desc: 'E a vida eterna. João 3:16',
+        url: 'https://i.pinimg.com/videos/thumbnails/originals/cf/0d/1d/cf0d1dfb8c66f25845631a6013db0adf.0000000.jpg',
+        video: 'https://v1.pinimg.com/videos/mc/720p/cf/0d/1d/cf0d1dfb8c66f25845631a6013db0adf.mp4'
+    },
+    {
+        id: 12, 
+        title: 'Jesus está voltando',
+        desc: 'E você está preparado?',
+        url: 'https://i.pinimg.com/videos/thumbnails/originals/d1/78/59/d17859245484a6db67a10d2df862b621.0000000.jpg',
+        video: 'https://br.pinterest.com/pin/576953402292287108/'
     },
 ]
