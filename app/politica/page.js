cbb1530df84ee7cd76f0ebc861977431031bd4b2
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-22 03:32:12 -0300

    politica

---
+++ app/politica/page.js
@@ -0,0 +1,61 @@
+export default function PoliticaPage() {
+    return (
+        <body>
+            Até que Ele venha Política de privacidade
+            Esta Política de privacidade descreve como suas informações pessoais são coletadas, usadas e compartilhadas quando você visita ou faz umacompra no https://atequeelevenha.com (o  Site ).
+            INFORMAÇÕES PESSOAIS QUE COLETAMOS
+            <p> 
+            Quando você visita o Site, coletamos automaticamente determinadas informações sobre o seu dispositivo, incluindo informações sobre seu
+            navegador da web, endereço IP, fuso horário e de alguns dos cookies instalados no seu dispositivo. Além disso, à medida que você navega no
+            Site, coletamos informações sobre as páginas da web ou produtos individuais que você visualiza, quais sites ou termos de pesquisa que
+            indicaram o Site a você e informações sobre como você interage com o Site. Referimo-nos a essa informação coletada automaticamente como Informações do dispositivo .
+            Coletamos Informações do dispositivo usando as seguintes tecnologias:
+            </p> 
+            
+            COOKIES
+
+            <p>
+            Segue uma lista dos cookies que usamos. Nós os listamos para que você possa escolher se você quer optar pelos cookies.
+            _session_id, token único, por sessão, Permite a armazenar informações sobre sua sessão (referenciador, landing page, etc.)
+         
+            Além disso, quando você faz uma compra ou tenta fazer uma compra no Site, nós coletamos algumas informações suas, incluindo seu nome,
+            endereço de faturamento, endereço de entrega, informações de pagamento (incluindo números de cartão de crédito), endereço de e-mail e número de telefone. Nós chamamos essas informações de “Informações do
+            pedido”.
+ 
+            Quando falamos sobre “Informações pessoais” nesta Política de privacidade, estamos falando sobre Informações do dispositivo e Informações
+            do pedido.
+            </p>
+
+            COMO USAMOS SUAS INFORMAÇÕES PESSOAIS?
+
+            <p>
+
+            Em geral, nós usamos as Informações do pedido que coletamos para processar quaisquer pedidos feitos por meio do Site (incluindo o
+            processamento das suas informações de pagamento, a organização do frete e o fornecimento de faturas e/ou confirmações de pedidos). Além disso, usamos essas Informações do pedido para:
+            nos comunicarmos com você;
+           
+            </p>
+       
+
+            NÃO RASTREAR
+            <p>
+            Por favor, observe que nós não alteramos a coleta de dados e as práticas de uso do nosso Site quando vemos um sinal de Não rastrear no seu navegador.
+            </p>
+          
+
+            MUDANÇAS
+            <p>
+            Nós podemos atualizar esta Política de privacidade periodicamente para refletir, por exemplo, mudanças em nossas práticas ou por outros
+            motivos operacionais, legais ou regulamentares.
+            </p>
+
+
+            FALE CONOSCO
+            <p>
+            Para saber mais sobre nossas práticas de privacidade, caso tenha dúvidas ou se quiser fazer uma reclamação, entre em contato conosco por email em joaodesousa101@gmail.com ou pelo correio usando as informações abaixo:
+            Rua Rudolfo Huffenuesler, Jaraguá do Sul, SC, 89251300, Brasil
+            </p>
+        </body>
+    )
+
+}
