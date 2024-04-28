b04848001995e3a72475ea17bf26042eadbb992d
JohnnyBoySou <joaodesousa101@gmail.com>
2024-04-28 16:12:08 -0300

    day page

--- src/app/day/page.js
+++ src/app/day/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { Column, Row, Main, Title, Label, } from '@theme/global';
+import { Column, Row, Main, Title, Label, } from '../../theme/global';
 export default function Day(){
 return(
     <Main>
