2367ff165584a27b1d5f98ed07fdccc2d57ef57e
JohnnyBoySou <joaodesousa101@gmail.com>
2024-05-14 16:46:03 -0300

    add new endpoint

---
+++ src/data/playlists/music.js
@@ -0,0 +1,39 @@
+export const Playlists = [
+    {
+        id: 1,
+        title: "Gospel Acústico",	
+        image: "https://i.pinimg.com/736x/bd/84/61/bd8461a782dcddee77f3a6b522db38ce.jpg",
+        tag: "Relaxar",
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DX7O8ONc6Mo0L'
+    },
+    {
+        id: 2, 
+        title: "Trap Gospel",
+        image: 'https://i.pinimg.com/736x/5d/04/1b/5d041b13cb1f2846d1a8293cff7e0ad2.jpg',
+        tag: 'Trap',
+        link: 'https://open.spotify.com/playlist/5gC0k5KwSVG7aVENGDkpCG',
+    },
+    {
+        id: 3,
+        title: 'Pop Gospel',
+        image: 'https://i.pinimg.com/736x/a6/51/98/a6519896ebd1aff3c7f0efe107587521.jpg',
+        tag: 'Pop',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DWXia8adWhIq7',
+    },
+    {
+        id: 4,
+        title: 'Rock Gospel',
+        image: 'https://i.pinimg.com/736x/2f/e4/25/2fe425fe050e122a374a059532f27254.jpg',
+        tag: 'Rock',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DWZ0BlvV9acBN',
+    },
+    {
+        id: 5,
+        title: 'Festa Gospel',
+        image: 'https://i.pinimg.com/736x/b4/5a/bd/b45abd1e6a4d24942a4fc0ccec63ee3b.jpg',
+        tag: 'Festa',
+        link: 'https://open.spotify.com/playlist/37i9dQZF1DXbHyOLBdwV5A',
+    },
+
+
+]
